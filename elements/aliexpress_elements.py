66c3e6c0e66bea0ee5c2751aef3caed41571fdce
Carlos Cadenas <charleschains@gmail.com>
2020-02-25 19:39:30 -0300

    Created elements to be used by the feature

---
+++ elements/aliexpress_elements.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+
+results_list = (By.CSS_SELECTOR, "a[class='item-title']")
+product_quantity = (By.CSS_SELECTOR, "div[class='product-quantity-tip']")
+search_button = (By.CSS_SELECTOR, "input[type='submit']" )
+search_field = (By.ID, "search-cate")
+popup = (By.CSS_SELECTOR, "div[class*='closeable']")
+popup_cross = (By.CSS_SELECTOR, "a[role='button']")
