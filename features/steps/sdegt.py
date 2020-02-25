6457df4bdd77a3829ade3d1a5cd10179843af147
Carlos Cadenas <charleschains@gmail.com>
2020-02-25 19:42:53 -0300

    Added step to close the popup

--- features/steps/sdegt.py
+++ features/steps/sdegt.py
@@ -1,25 +1,31 @@
 from behave import *
-from ../elements import *
+from elements import aliexpress_elements as elements
 
 @given('I access "http://www.aliexpress.com/"')
 def step_impl(context):
 	context.driver.get("http://www.aliexpress.com/")
 
-@given('I search for "iphone"")
+@given('I search for "iphone"')
 def step_imp(context):
-	get_element(context.elements, search_field).send_keys("iphone")
+	get_element(context.driver, elements.search_field).send_keys("iphone")
 	#TODO: Change given to include the search action
-	get_element(context.elements.Search_button).click()
+	get_element(context.driver, elements.search_button).click()
 
-@when('I enter the second item in the search results")
+@given('I close the popup')
 def step_imp(context):
-	get_elements(context.elements, results_list)[1].click()
+	popup = get_element(context.driver, elements.popup)
+	get_element(popup, elements.popup_cross).click()
+
+
+@when('I enter the second item in the search results')
+def step_imp(context):
+	get_elements(context.driver, elements.results_list)[1].click()
 	context.driver.switch_to.window(window_handles[1])
 
 @then('I see there are more than 1 piece available')
 def step_imp(context):
-	quantity_found = get_element(context.elements.product_quantity).split(" ")[0]
-	assert(int(quantity_found > 1, "The quantity of items to be bought is less than 1 ({})".format(quantity_found)
+	quantity_found = get_element(context.driver, elements.product_quantity).split(" ")[0]
+	assert(int(quantity_found) > 1, "The quantity of items to be bought is less than 1 ({})".format(quantity_found))
 
 
 """  Scenario: "As a Customer we want to see if the second Iphone related ad from the second results page from www.aliexpress.com has at least 1 item to be bought."
