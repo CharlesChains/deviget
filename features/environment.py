8b41634404bd98ff586b0ff3398d4a37f183e310
Carlos Cadenas <charleschains@gmail.com>
2020-02-26 15:27:01 -0300

    Changed driver to use relative path

--- features/environment.py
+++ features/environment.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
+import os
+
 
 def before_all(context):
-	path = "../chromedriver"
+	path = ("../deviget/chromedriver/chromedriver")
+	os.chmod(path, 0o755)
 
-	context.driver = webdriver.Chrome(path
-	)
+	context.driver = webdriver.Chrome(executable_path=os.path.realpath(path))
 
 def after_all(context):
 	context.driver.quit()
