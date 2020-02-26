d82be0bdd93105ca695e6de961062b0622697189
Carlos Cadenas <charleschains@gmail.com>
2020-02-26 15:27:52 -0300

    Added the step to go to second page

--- features/sdet-aliexpress-test.feature
+++ features/sdet-aliexpress-test.feature
@@ -1,8 +1,8 @@
 Feature: SDET--SG
-
-  Scenario: "As a Customer we want to see if the second Iphone related ad from the second results page from www.aliexpress.com has at least 1 item to be bought."
-	Given I access "http://www.aliexpress.com/"
-	Given I search for "iphone"
-	When I enter the second item in the search results
-  	Given  I close the popup
-	Then I see there are more than 1 piece available
+	Scenario: "As a Customer we want to see if the second Iphone related ad from the second results page from www.aliexpress.com has at least 1 item to be bought."
+		Given I access "http://www.aliexpress.com/"
+		Given I search for "iphone"
+		Given I go to the 2nd page
+		When I enter the second item in the search results
+		Given I close the popup
+		Then I see there are more than 1 piece available
--- features/steps/sdegt.py
+++ features/steps/sdegt.py
@@ -1,6 +1,11 @@
 from behave import *
 from elements import aliexpress_elements as elements
 
+def get_element(context, by):
+	return context.find_element(*by)
+def get_elements(context,by):
+	return context.find_elements(*by)
+
 @given('I access "http://www.aliexpress.com/"')
 def step_impl(context):
 	context.driver.get("http://www.aliexpress.com/")
@@ -19,12 +24,12 @@ def step_imp(context):
 @when('I enter the second item in the search results')
 def step_imp(context):
 	get_elements(context.driver, elements.results_list)[1].click()
-	context.driver.switch_to.window(window_handles[1])
+	context.driver.switch_to.window(context.driver.window_handles[1])
 
 @then('I see there are more than 1 piece available')
 def step_imp(context):
 	quantity_found = get_element(context.driver, elements.product_quantity).split(" ")[0]
-	assert(int(quantity_found) > 1, "The quantity of items to be bought is less than 1 ({})".format(quantity_found))
+	assert(int(quantity_found) > 1)
 
 
 """  Scenario: "As a Customer we want to see if the second Iphone related ad from the second results page from www.aliexpress.com has at least 1 item to be bought."
@@ -32,3 +37,8 @@ def step_imp(context):
 	Given I enter "Iphone" into the search field
 	When I enter the second item in the search results
 	Then I see there are more than 1 piece available"""
+
+
+@given("I go to the 2nd page")
+def step_impl(context):
+	get_element(context.driver, elements.second_page).click()
